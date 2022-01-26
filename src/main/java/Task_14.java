23f5ba562d25e9f1457015656c3598c0712ff1a4
Vitaliy A. Yudin <vyudin@t1-consulting.ru>
2022-01-26 10:53:44 +0300

    doc refactoring

--- src/main/java/Task_14.java
+++ src/main/java/Task_14.java
@@ -7,22 +7,16 @@ import java.util.stream.LongStream;
 
 /**
  * Longest Collatz sequence
- * Отправить
- * <p>
  * Show HTML problem content
  * Problem 14
  * The following iterative sequence is defined for the set of positive integers:
- * <p>
  * n → n/2 (n is even)
  * n → 3n + 1 (n is odd)
- * <p>
  * Using the rule above and starting with 13, we generate the following sequence:
- * <p>
  * 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
- * It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
- * <p>
+ * It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
+ * Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
  * Which starting number, under one million, produces the longest chain?
- * <p>
  * NOTE: Once the chain starts the terms are allowed to go above one million.
  */
 public class Task_14 {
