a8ac1e5f814c14e71defade189019c8fd17a3099
Vitaliy A. Yudin <vyudin@t1-consulting.ru>
2022-12-28 14:20:01 +0300

    rename task 15 to task 16

--- src/main/java/Task_16.java
+++ src/main/java/Task_16.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
  * What is the sum of the digits of the number 2^1000?
  */
-public class Task_15 {
+public class Task_16 {
     public static void main(String[] args) {
         // Вычисляем число 2^1000
         BigDecimal multiplyResult = new BigDecimal(2).pow(1000);
